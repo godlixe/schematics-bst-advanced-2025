63237ae29761b7103b0e50fe4727f93ab7e9c69c
Alexander <alexander@Alexanders-MacBook-Pro.local>
2025-09-27 00:32:13 +0700

    fix: foreign key

--- day3/contoh-2/model/comment.go
+++ day3/contoh-2/model/comment.go
@@ -5,9 +5,9 @@ import "time"
 type Comment struct {
 	ID        int       `json:"id"`
 	UserID    int       `json:"user_id"`
-	User      *User     `json:"user,omitempty" gorm:"constraint:OnUpdate:CASCADE;OnDelete:CASCADE;"`
+	User      *User     `json:"user,omitempty" gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE;"`
 	BlogID    int       `json:"blog_id"`
-	Blog      *Blog     `json:"blog,omitempty" gorm:"constraint:OnUpdate:CASCADE;OnDelete:CASCADE;"`
+	Blog      *Blog     `json:"blog,omitempty" gorm:"foreignKey:BlogID;references:ID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE;"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
